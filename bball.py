7325b53a48f5c208b5a1b909f7e00c2b47a03494
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-04 07:26:32 +0100

    Refactored code: added 2 helper functions to check for key_down and key_up events

--- bball.py
+++ bball.py
@@ -4,10 +4,10 @@ from settings import Settings
 from bar import Bar
 
 class BouncingBall:
-    """Overall class to manage game assets and behavior"""
+    """Overall class to manage game assets and behavior."""
 
     def __init__(self):
-        """Initialize the game, and create game resources"""
+        """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
@@ -16,7 +16,7 @@ class BouncingBall:
         self.bar = Bar(self)
 
     def run_game(self):
-        """Main game loop"""
+        """Main game loop."""
         while True:
             self._check_events()
             self.bar.update() # implements movement depending on movement flag
@@ -29,17 +29,24 @@ class BouncingBall:
                 sys.exit()
             # Pressing arrow keys for movement:
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.bar.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.bar.moving_left = True
+                self._check_keydown_events(event)
             # Releasing arrow keys:
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.bar.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.bar.moving_left = False
+                self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event):
+        """Checks keydown events."""
+        if event.key == pygame.K_RIGHT:
+            self.bar.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.bar.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Checks keyup events."""
+        if event.key == pygame.K_RIGHT:
+            self.bar.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.bar.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
