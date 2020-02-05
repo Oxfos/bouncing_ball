e60ac4ca01f02492dec19d4932dc8c4b894c3399
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-05 07:41:11 +0100

    Updated screen size

--- bball.py
+++ bball.py
@@ -10,7 +10,8 @@ class BouncingBall:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+            self.settings.screen_height))
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Bouncing Ball")
